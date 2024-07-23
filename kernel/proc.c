98672478ff5e9f6ec1c21d832823ed372eaf89cf
Junru Wang <peterWangharuko@gmail.com>
2024-07-23 17:39:14 +0800

    Complete USYSCALL and print page table

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,13 @@ found:
     return 0;
   }
 
+  // Allocate a USYSCALL page
+  if ((p->kupage = (struct usyscall *)kalloc()) == 0) {
+      freeproc(p);
+      release(&p->lock);
+      return 0;
+  }
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -140,6 +147,7 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
+  p->kupage->pid = p->pid;
 
   return p;
 }
@@ -164,6 +172,7 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  p->kupage = 0;
 }
 
 // Create a user page table for a given process,
@@ -196,6 +205,14 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the USYSCALL just below TRAPFRAME
+  if (mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->kupage), PTE_R | PTE_U) < 0) {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +223,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
